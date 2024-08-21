0d83db384b5157980fc8058255c396f49098bd67
Morten Lied Johansen <morten.lied.johansen@nav.no>
2024-08-21 21:46:08 +0200

    Tests for SSE API

--- pkg/election/official/official_test.go
+++ pkg/election/official/official_test.go
@@ -2,6 +2,7 @@ package official
 
 import (
 	"context"
+	"fmt"
 	. "github.com/benjamintf1/unmarshalledmatchers"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -70,6 +71,57 @@ var _ = Describe("Official", func() {
 			Expect(res.Header.Get("Content-Type")).To(Equal("application/json"))
 			Expect(res.Header.Get("Cache-Control")).To(Equal("no-cache"))
 			Expect(io.ReadAll(res.Body)).To(ContainUnorderedJSON(`{"name":"new result"}`))
+		})
+	})
+
+	Context("sse api", func() {
+		var w *httptest.ResponseRecorder
+
+		BeforeEach(func() {
+			var cancel context.CancelFunc
+			ctx, cancel = context.WithTimeout(ctx, 100*time.Millisecond)
+			DeferCleanup(cancel)
+
+			w = httptest.NewRecorder()
+			o.lastResult = result{
+				Name:       "last result",
+				LastUpdate: "then",
+			}
+		})
+
+		It("should return initial election result", func() {
+			go o.sseHandler(ctx)(w, nil)
+			time.Sleep(10 * time.Millisecond)
+
+			line, err := w.Body.ReadString('\n')
+			Expect(err).ToNot(HaveOccurred())
+			Expect(line).To(HavePrefix("data: "))
+			data := line[6:]
+			Expect(data).To(MatchJSON(`{"name":"last result","last_update":"then"}`))
+		})
+
+		It("should continue to update election results", func() {
+			go o.sseHandler(ctx)(w, nil)
+			time.Sleep(10 * time.Millisecond)
+
+			line, err := w.Body.ReadString('\n')
+			Expect(err).ToNot(HaveOccurred())
+			Expect(line).To(HavePrefix("data: "))
+			data := line[6:]
+			Expect(data).To(MatchJSON(`{"name":"last result","last_update":"then"}`))
+			Expect(w.Body.ReadString('\n')).To(Equal("\n"))
+
+			for _, result := range []string{"first result", "second result", "third result"} {
+				electionResults <- result
+				time.Sleep(10 * time.Millisecond)
+
+				line, err := w.Body.ReadString('\n')
+				Expect(err).ToNot(HaveOccurred())
+				Expect(line).To(HavePrefix("data: "))
+				data := line[6:]
+				Expect(data).To(ContainUnorderedJSON(fmt.Sprintf(`{"name":"%s"}`, result)))
+				Expect(w.Body.ReadString('\n')).To(Equal("\n"))
+			}
 		})
 	})
 })
